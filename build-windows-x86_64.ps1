4b41208bf65756f9da5fbe85a4b648867283f323
Minyewoo <const.trushov@gmail.com>
2024-04-18 15:32:10 +0300

    Added windows x86_64 build process

---
+++ build-windows-x86_64.ps1
@@ -0,0 +1,4 @@
+Invoke-WebRequest "https://downloads.sourceforge.net/project/snap7/1.4.2/snap7-full-1.4.2.7z" -OutFile ".\snap7-full-1.4.2.7z"
+7z x ".\snap7-full-1.4.2.7z"
+cd ".\snap7-full-1.4.2\build\windows\MINGW64"
+make all
