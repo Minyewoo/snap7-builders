b5869f4c79a983932d145cea2f57a4f066661bd8
Minyewoo <const.trushov@gmail.com>
2024-04-18 12:56:31 +0300

    Unpiped compression

--- build-linux-x86_64.sh
+++ build-linux-x86_64.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-wget https://downloads.sourceforge.net/project/snap7/1.4.2/snap7-full-1.4.2.7z | 7z x snap7-full-1.4.2.7z
+wget https://downloads.sourceforge.net/project/snap7/1.4.2/snap7-full-1.4.2.7z
+7z x snap7-full-1.4.2.7z
 cd ./snap7-full-1.4.2/build/unix
 make -f x86_64_linux.mk all
