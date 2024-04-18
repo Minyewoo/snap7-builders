c68993b5bf7c62914205700d79c9ac326ed52654
Minyewoo <const.trushov@gmail.com>
2024-04-18 16:38:36 +0300

    Fixed windows archive download

--- build-windows-x86_64.ps1
+++ build-windows-x86_64.ps1
@@ -1,4 +1,4 @@
 Invoke-WebRequest -UserAgent "Wget" -Uri "https://downloads.sourceforge.net/project/snap7/1.4.2/snap7-full-1.4.2.7z" -OutFile "snap7-full-1.4.2.7z"
 7z x "snap7-full-1.4.2.7z"
 cd ".\snap7-full-1.4.2\build\windows\MINGW64"
-make.bat all
+cmd.exe /c make.bat all
