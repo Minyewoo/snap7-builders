227d584e09f12eae24c45980ce98d170dbfeb186
Minyewoo <const.trushov@gmail.com>
2024-04-18 17:11:21 +0300

    Adapted macos build script

--- build-macos-aarch64.sh
+++ build-macos-aarch64.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
-wget https://downloads.sourceforge.net/project/snap7/1.4.2/snap7-full-1.4.2.7z
-7z x snap7-full-1.4.2.7z
-cp aarch64_osx.mk ./snap7-full-1.4.2/build/osx/
-cd ./snap7-full-1.4.2/build/osx
+curl -L http://127.0.0.1:8000 -o snap7-full-1.4.2.7z
+7za x snap7-full-1.4.2.7z
+cp aarch64_osx.mk ./snap7-full-1.4.2/build/osx/aarch64_osx.mk
+cd ./snap7-full-1.4.2/build/osx/
 make -f aarch64_osx.mk all
