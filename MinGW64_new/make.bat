fe099407fe8d3236a8050aa5e6dddafc373336c7
Minyewoo <const.trushov@gmail.com>
2024-04-18 17:56:37 +0300

    Windows build workarounds

---
+++ MinGW64_new/make.bat
@@ -0,0 +1,2 @@
+path=C:\ProgramData\mingw64\mingw64\bin;C:\ProgramData\mingw64\mingw64\x86_64-w64-mingw32\bin;%PATH
+mingw32-make %1
--- build-windows-x86_64.ps1
+++ build-windows-x86_64.ps1
@@ -1,6 +1,5 @@
-# Invoke-WebRequest -UserAgent "Wget" -Uri "https://downloads.sourceforge.net/project/snap7/1.4.2/snap7-full-1.4.2.7z" -OutFile "snap7-full-1.4.2.7z"
-# 7z x "snap7-full-1.4.2.7z"
-# cd ".\snap7-full-1.4.2\build\windows\MINGW64"
-tree C:\ProgramData\mingw64 /F
-tree C:\ProgramData\mingw32 /F
-# cmd.exe /c mingw32-make all
+Invoke-WebRequest -UserAgent "Wget" -Uri "https://downloads.sourceforge.net/project/snap7/1.4.2/snap7-full-1.4.2.7z" -OutFile "snap7-full-1.4.2.7z"
+7z x "snap7-full-1.4.2.7z"
+Copy-Item -Path ".\MinGW64_new" -Destination ".\snap7-full-1.4.2\build\windows\MinGW64_new" -Recurse
+cd ".\snap7-full-1.4.2\build\windows\MinGW64_new"
+cmd.exe /c mingw32-make all
