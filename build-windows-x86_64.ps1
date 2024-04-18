fad715cac92faa740c83be29e1a69f962e41a61a
Minyewoo <const.trushov@gmail.com>
2024-04-18 17:44:55 +0300

    Windows test

--- build-windows-x86_64.ps1
+++ build-windows-x86_64.ps1
@@ -1,5 +1,6 @@
 # Invoke-WebRequest -UserAgent "Wget" -Uri "https://downloads.sourceforge.net/project/snap7/1.4.2/snap7-full-1.4.2.7z" -OutFile "snap7-full-1.4.2.7z"
 # 7z x "snap7-full-1.4.2.7z"
 # cd ".\snap7-full-1.4.2\build\windows\MINGW64"
-cmd.exe /c where mingw32-make.exe
+tree C:\ProgramData\mingw64 /F
+tree C:\ProgramData\mingw32 /F
 # cmd.exe /c mingw32-make all
