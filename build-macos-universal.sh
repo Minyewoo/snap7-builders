d2e14ec7893272e3d5c6a8b126e2ebe0e54bcd0e
Minyewoo <const.trushov@gmail.com>
2024-04-18 18:47:43 +0300

    Macos Rosetta 2 workarounds

--- build-macos-universal.sh
+++ build-macos-universal.sh
@@ -4,7 +4,7 @@ curl -L https://downloads.sourceforge.net/project/snap7/1.4.2/snap7-full-1.4.2.7
 cp aarch64_osx.mk ./snap7-full-1.4.2/build/osx/aarch64_osx.mk
 cd ./snap7-full-1.4.2/build/osx/
 make -f aarch64_osx.mk all
-make -f x86_64_osx.mk all
+arch -x86_64 make -f x86_64_osx.mk all
 cd ..
 mkdir ./bin/universal-osx
 lipo ./bin/x86_64-osx/libsnap7.dylib ./bin/aarch64-osx/libsnap7.dylib -output ./bin/universal-osx/libsnap7.dylib -create
